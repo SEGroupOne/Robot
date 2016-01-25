60477372a9102223ca5c89b0facde5ffda298a59
roadfar <839377654@qq.com>
2016-01-25 17:06:40 +0800

    delete git info
    delete git info

--- measure.py
+++ measure.py
@@ -10,10 +10,7 @@ logging.basicConfig(level=logging.DEBUG,
                 filename='sonar.log',
                 filemode='w')
 
-<<<<<<< HEAD
 logging.debug("this is the message !___test changed")
-=======
->>>>>>> master
 
 //测试时间
 #put commits logs
